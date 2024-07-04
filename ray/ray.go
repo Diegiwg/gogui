fda20f26b758d19ccb04552dea47779e4587ab95
Diegiwg <diegiwg@gmail.com>
2024-07-04 13:42:51 -0300

    feat: update rayblib files to only .a and .h

--- ray/ray.go
+++ ray/ray.go
@@ -1,9 +1,7 @@
 package gogui_raylib
 
 /*
-#cgo CFLAGS:  -I/data/hdd1/gogui/ray/raylib-5.0_linux_amd64/include
-#cgo LDFLAGS: -L/data/hdd1/gogui/ray/raylib-5.0_linux_amd64/lib -Wl,-rpath -Wl,/data/hdd1/gogui/ray/raylib-5.0_linux_amd64/lib
-#cgo LDFLAGS: -L/data/hdd1/gogui/ray/raylib-5.0_linux_amd64/lib/libraylib.a -lraylib -lGL -lm -lpthread -ldl -lrt
+#cgo LDFLAGS: -L\. -lraylib -lGL -lm -lpthread -ldl -lrt
 
 #include <stdlib.h>
 #include "raylib.h"
